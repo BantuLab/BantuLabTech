5a4da391d07519f02c9e9ceccbbf56cc4f288e0f
Chaiwa Berian <chaiwa.berian@gmail.com>
2018-10-01 22:54:12 +0200

    Cleanups

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <template>
   <v-app >
     <v-toolbar app>
-      <div class="container">
-        <span></span><router-link to="/" class="brand-logo">
-        BantuLabTech
-        </router-link>
-      </div>
+      <!-- <div class="container" fluid> -->
+       <!-- <v-layout column align-center> -->
+        <!-- <v-toolbar-side-icon> -->
+          <!-- <router-link to="/" class="brand-logo" >
+            <img src="@/assets/bantulab_logo.png" class="mb-5">
+          </router-link> -->
+          <!-- <img src="@/assets/bantulab_logo.png"> -->
+        <!-- </v-toolbar-side-icon> -->
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+          <v-icon large color="blue-grey darken-2">call_split</v-icon>
+        <v-toolbar-title >BantuLabTech</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>search</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>apps</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>refresh</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+       <!-- </v-layout> -->
+      <!-- </div> -->
     </v-toolbar>
      <v-content> 
         <v-container fluid>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,16 @@
 <template>
     <v-container fluid>
-        <h1>Welcome to BantuLab Technology Innovation Lab</h1>
-        <v-btn :to="{name: 'register-member'}" class="primary">
-            Register as a member!
-        </v-btn>
+        <h2  class="text-xs-center">Welcome to BantuLab Technology Innovation Lab</h2>
+        <br/>
+        <div class="text-xs-center">
+            <v-btn v-on:click="gotoSlack" target="_blank"  class="error">Join Tech Saavy Community</v-btn> 
+            <!-- <div> -->
+                <em class="text-xs-center">OR</em>
+            <!-- </div> -->
+            <v-btn :to="{name: 'register-member'}" class="primary">
+                Register as a member
+            </v-btn>
+        </div>
         
     </v-container>
     
@@ -20,6 +27,12 @@
         name: 'Home',
         components: {
 
+        },
+        methods: {
+            gotoSlack(){
+                // window.location.href = "http://bit.ly/BantuLabTechSlack"
+                window.open('http://bit.ly/BantuLabTechSlack', '_blank')
+            }
         }
     }
 </script>
